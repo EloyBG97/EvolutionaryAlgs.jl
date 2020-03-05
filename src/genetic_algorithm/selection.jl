43a49d3b1e355c03a9211fc6d9fd8830f7405983
Eloy Bedia Garcia <eloybg97@correo.ugr.es>
2020-03-05 19:09:46 +0000

    Añadidos linear_selection y random_selection

--- src/genetic_algorithm/selection.jl
+++ src/genetic_algorithm/selection.jl
@@ -1,3 +1,14 @@
+using Random
+
+"""
+For each gen, is been assigned a probability to be chosen by the next formula:
+
+``\frac{ffitness(gen)}∑^n_{i=0}fitness(gen_i)``
+
+population ->\n
+ffitnes -> fitness function\n
+prop -> by default 0.5\n
+"""
 function roulette_wheel_selection(population::Array{T,2}, ffitness::Function, prop::Float64 = 0.5) where {T <: Any}
     gensize = size(population, 2)
     popsize = size(population, 1)
@@ -15,11 +26,37 @@ function roulette_wheel_selection(population::Array{T,2}, ffitness::Function, pr
             selectedpop = vcat(selectedpop, gen)
             last = last + 1
 
-            if last == prop * popsize
+            if last == floor(prop * popsize)
                 break
             end
         end
     end
 
     return selectedpop
+end
+
+"""
+Select the 1/prop best part from the population\n
+population ->\n
+ffitnes -> fitness function\n
+prop -> by default 0.5\n
+"""
+function linear_selection(population::Array{T,2}, ffitness::Function, prop::Float64 = 0.5) where {T <: Any}
+    sort_pop = sort(population, dims = 1, by = ffitness, rev = true)
+    popsize = size(population, 1)
+
+    sizeselected = convert(UInt64, floor(prop * popsize))
+
+    sort_pop[1:sizeselected,:]
+end
+
+function random_selection(population::Array{T,2}, ffitness::Function, prop::Float64 = 0.5) where {T <: Any}
+    popsize = size(population, 1)
+    sizeselected = convert(Int64, floor(prop * popsize))
+
+    perm = randperm(popsize)
+
+    selectedpop = population[perm, :]
+
+    return selectedpop[1:sizeselected, :]
 end
--- test/genetic_algorithm/selection_test.jl
+++ test/genetic_algorithm/selection_test.jl
@@ -2,7 +2,7 @@ using EvolutionaryAlgs
 using Test
 
 
-@testset "genetic_algorithm/selection.jl" begin
+@testset "roulette_wheel_selection" begin
     @test begin
         population = rand(40, 20)
         selectedpop = EvolutionaryAlgs.roulette_wheel_selection(population, sum)
@@ -11,10 +11,10 @@ using Test
     end
 
     @test begin
-        population = rand(40, 20)
+        population = rand(43, 20)
         selectedpop = EvolutionaryAlgs.roulette_wheel_selection(population, sum, 0.75)
 
-        size(selectedpop, 1) == 30 && size(selectedpop, 2) == 20
+        size(selectedpop, 1) == 32 && size(selectedpop, 2) == 20
     end
 
     @test begin
@@ -25,8 +25,68 @@ using Test
     end
 
     @test begin
-        population = rand(500, 75)
+        population = rand(503, 75)
         selectedpop = EvolutionaryAlgs.roulette_wheel_selection(population, sum, 0.75)
+
+        size(selectedpop, 1) == 377 && size(selectedpop, 2) == 75
+    end
+end
+
+@testset "linear_selection" begin
+    @test begin
+        population = rand(40, 20)
+        selectedpop = EvolutionaryAlgs.linear_selection(population, sum)
+
+        size(selectedpop, 1) == 20 && size(selectedpop, 2) == 20
+    end
+
+    @test begin
+        population = rand(45, 20)
+        selectedpop = EvolutionaryAlgs.linear_selection(population, sum, 0.75)
+
+        size(selectedpop, 1) == 33 && size(selectedpop, 2) == 20
+    end
+
+    @test begin
+        population = rand(129, 40)
+        selectedpop = EvolutionaryAlgs.linear_selection(population, sum)
+
+        size(selectedpop, 1) == 64 && size(selectedpop, 2) == 40
+    end
+
+    @test begin
+        population = rand(500, 75)
+        selectedpop = EvolutionaryAlgs.linear_selection(population, sum, 0.75)
+
+        size(selectedpop, 1) == 375 && size(selectedpop, 2) == 75
+    end
+end
+
+@testset "random_selection" begin
+    @test begin
+        population = rand(40, 20)
+        selectedpop = EvolutionaryAlgs.random_selection(population, sum)
+
+        size(selectedpop, 1) == 20 && size(selectedpop, 2) == 20
+    end
+
+    @test begin
+        population = rand(45, 20)
+        selectedpop = EvolutionaryAlgs.random_selection(population, sum, 0.75)
+
+        size(selectedpop, 1) == 33 && size(selectedpop, 2) == 20
+    end
+
+    @test begin
+        population = rand(129, 40)
+        selectedpop = EvolutionaryAlgs.random_selection(population, sum)
+
+        size(selectedpop, 1) == 64 && size(selectedpop, 2) == 40
+    end
+
+    @test begin
+        population = rand(500, 75)
+        selectedpop = EvolutionaryAlgs.random_selection(population, sum, 0.75)
 
         size(selectedpop, 1) == 375 && size(selectedpop, 2) == 75
     end
